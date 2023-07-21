ea7afd9c903069b6c70d51eed3217d63493b82f4
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-07-21 09:49:46 +0600

    Getting video route

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,10 +1,11 @@
+const { addVideo, getAllVideos } = require('../controllers/videoController')
 const { videoUpload } = require('../middlewares/videoUpload')
 
 const router = require('express').Router()
 
 
-router.post('/upload',videoUpload.single('video'), (req,res)=>{
-    res.send({message:'Video Uploaded'})
-})
+router
+.post('/upload',videoUpload.single('video'), addVideo)
+.get('/videos', getAllVideos)
 
 module.exports = router
